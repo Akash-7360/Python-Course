29a2b5bae8180e79a454098e9cc439307daa3c82
Akash-7360 <akashgoud7360@gmail.com>
2025-09-08 21:28:49 +0530

    2nd Upload

---
+++ 11_functions/5_functionscope.py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    #a and b are local variables
+    c = a+b
+    return c
+
+def greet():
+    z = 32
+    print("Hello")
+   
+    return z
+
+z=8
+print(z)
+
+
+
+print(sum(4,6))
+print(greet())
---
+++ 11_functions/6_docstring.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    '''This will sum two numbers''' #this is doc string
+    c = a+b
+    return c
+
+print(sum.__doc__)
+print(sum(2,3))
+print()
